6831095035b4058bc4537ee970ab5dda22e361da
Pritam Mandal <pritam.mandal21072002@gmail.com>
2025-02-04 23:19:08 +0530

    uploding the consept of veriables by a code

---
+++ VARIABLES_DATA_TYPE/variables.java
@@ -0,0 +1,8 @@
+public class variables{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
