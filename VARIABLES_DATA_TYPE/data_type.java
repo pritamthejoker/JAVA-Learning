078cd900ba7ea29e071c5b0af81c6a92cb4a1209
Pritam Mandal <pritam.mandal21072002@gmail.com>
2025-02-04 23:26:40 +0530

    uploding the veraity of data type in java in a code

---
+++ VARIABLES_DATA_TYPE/data_type.java
@@ -0,0 +1,23 @@
+public class data_type{
+    public static void main(String args[]){
+        byte a = 2;
+        System.out.println(a);
+        short b = 40;
+        System.out.println(b);
+        char c = 'p';
+        System.out.println(c);
+        boolean d = true;
+        System.err.println(d);
+        int e = 5456;
+        System.out.println(e);
+        long f = 678544;
+        System.out.println(f);
+        float g = 3.4f;
+        System.out.println(g);
+        double i = 123.75;
+        System.out.println(i);
+        String j = "Pritam Mandal";
+        System.out.println(j);
+
+    }
+}
