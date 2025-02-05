684c02dc54829f30bfd1169e29adfe20fd0c142b
Pritam Mandal <pritam.mandal21072002@gmail.com>
2025-02-05 15:16:42 +0530

    Uploding a sample code to know the processes of taking input in java code

---
+++ VARIABLES_DATA_TYPE/input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        System.out.println(a);
+    }
+}
