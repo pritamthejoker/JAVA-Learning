06c64209dc1f527fb87434265682f5c95836e751
Pritam Mandal <pritam.mandal21072002@gmail.com>
2025-02-05 14:32:43 +0530

    Uploding the processes or comment in java code

---
+++ VARIABLES_DATA_TYPE/comment.java
@@ -0,0 +1,11 @@
+public class comment{
+    public static void main(String args[]){
+        //testing the single line comments
+
+        System.out.println("This is an exemple code for comments in java code");
+
+        /* testing the 
+        multi line 
+        comment */
+    }
+}
